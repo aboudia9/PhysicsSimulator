3c2baf575f6b44375b4c9a5cb93d0cdc14677c9f
aboudia9 <144206699+aboudia9@users.noreply.github.com>
2025-05-14 08:43:01 -0700

    Delete src/Physics2D/primitives directory
    Cleaning up the project structure

--- src/Physics2D/primitives/Box2D.java
+++
@@ -1,4 +0,0 @@
-package Physics2D.primitives;
-
-public class Box2D {
-}
--- src/Physics2D/primitives/Circle.java
+++
@@ -1,9 +0,0 @@
-package Physics2D.primitives;
-
-public class Circle {
-    private float radius;
-
-    public float getRadius() {
-        return this.radius;
-    }
-}
